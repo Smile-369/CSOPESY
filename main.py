3b3f6e2ba0b1bea1d1c4ee8b543f14d506a4c96f
Tiaro-hub <130729389+Tiaro-hub@users.noreply.github.com>
2024-02-27 23:37:58 +0800

    Update main.py
    Added SJF

--- main.py
+++ main.py
@@ -97,6 +97,59 @@ def fcfs_scheduling(processes):
         print(f" P{item} |", end="")
     print()
 
+#Shortest Job First
+def sjf_scheduling(processes):
+    sorted_processes = sorted(processes, key=lambda x: x[1])
+
+    completion_time = [0] * len(sorted_processes)
+    waiting_time = [0] * len(sorted_processes)
+    gantt_chart = []  # Array to store the Gantt chart
+    start_time = [0] * (len(sorted_processes) + 1)
+
+    current_time = 0  # Initialize the current time with 0
+
+    while sorted_processes:
+        eligible_processes = [p for p in sorted_processes if p[1] <= current_time]
+
+        if not eligible_processes:
+            current_time += 1
+            continue
+
+        shortest_job = min(eligible_processes, key=lambda x: x[2])
+        if shortest_job[0] == 0:
+            pro_index = shortest_job[0]
+        else:
+            pro_index = shortest_job[0] - 1
+
+
+        index = sorted_processes.index(shortest_job)
+
+        if current_time < sorted_processes[index][1]:
+            gantt_chart.extend([0] * (sorted_processes[index][1] - current_time))
+            current_time = sorted_processes[index][1]
+
+        gantt_chart.extend([sorted_processes[index][0]] * sorted_processes[index][2])
+        start_time[shortest_job[0]] = current_time  # Update start time for the next process
+        waiting_time[pro_index] = start_time[shortest_job[0]] - processes[pro_index][1]
+        current_time += sorted_processes[index][2]
+        completion_time[pro_index] = current_time
+
+        sorted_processes.pop(index)
+
+    average_waiting_time = sum(waiting_time) / len(waiting_time)
+
+    print("\nShortest Job First (SJF) Scheduling (Non-preemptive) Results:")
+    for i in range(len(completion_time)):
+        print(f"P[{processes[i][0]}] start time: {start_time[processes[i][0]]} end time: {completion_time[i]} | Waiting time: {waiting_time[i]}")
+
+    print("Average waiting time:", average_waiting_time)
+
+    print("\nGantt Chart:")
+    print("|", end="")
+    for item in gantt_chart:
+        print(f" P{item} |", end="")
+    print()
+    
 #Shortest Remaining Time First      
 def srtf_scheduling(processes):
     # Sort processes by arrival time
