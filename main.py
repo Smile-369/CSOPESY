37c801bfb7677f47fa01ac70f0593e92f032e2cd
Francisco Emmanuel Dumas <francisco_dumas@dlsu.edu.ph>
2024-03-02 19:12:10 +0800

    updated Round robin

--- main.py
+++ main.py
@@ -219,107 +219,62 @@ def srtf_scheduling(processes):
     print()
 
 #Round Robin
-def queueUpdation(queue, timer, arrival, n, maxProccessIndex):
-    zeroIndex = -1
-    for i in range(n):
-        if queue[i] == 0:
-            zeroIndex = i
-            break
+def round_robin_scheduling(processes, quantum):
+    remaining_burst_time = [process[2] for process in processes]
+    completion_time = [0] * len(processes)
+    waiting_time = [0] * len(processes)
+    gantt_chart = []  # Array to store the Gantt chart
 
-    if zeroIndex == -1:
-        return
-    if maxProccessIndex < n:
-        queue[zeroIndex] = maxProccessIndex + 1
-
-
-def checkNewArrival(timer, arrival, n, maxProccessIndex, queue):
-    if timer < arrival[n-1]:
-        newArrival = False
-        for j in range(maxProccessIndex+1, n):
-            if arrival[j] <= timer:
-                if maxProccessIndex < j:
-                    maxProccessIndex = j
-                    newArrival = True
-
-        # adds the index of the arriving process(if any)
-        if newArrival:
-            queueUpdation(queue, timer, arrival, n, maxProccessIndex)
-
-
-def queueMaintainence(queue, n):
-    for i in range(n-1):
-        if queue[i+1] != 0:
-            queue[i], queue[i+1] = queue[i+1], queue[i]
-
-
-def round_robin_scheduling(processes, tq):
-    n = len(processes)
-    avgWait, avgTT = 0, 0
-
-    arrival = [p[1] for p in processes]
-    burst = [p[2] for p in processes]
-    temp_burst = burst.copy()
-    wait = [0] * n
-    turn = [0] * n
-    complete = [False] * n
-    queue = [0] * n
-    maxProccessIndex = 0
-    timer = 0
-
+    current_time = 0
     while True:
-        flag = True
-        for i in range(n):
-            if temp_burst[i] != 0:
-                flag = False
-                break
-
-        if flag:
+        all_processes_completed = True
+        for i in range(len(processes)):
+            if remaining_burst_time[i] > 0:
+                all_processes_completed = False
+                if remaining_burst_time[i] > quantum:
+                    gantt_chart.extend([processes[i][0]] * quantum)
+                    current_time += quantum
+                    remaining_burst_time[i] -= quantum
+                else:
+                    gantt_chart.extend([processes[i][0]] * remaining_burst_time[i])
+                    current_time += remaining_burst_time[i]
+                    remaining_burst_time[i] = 0
+                    completion_time[i] = current_time
+                    waiting_time[i] = current_time - processes[i][1] - processes[i][2]
+        if all_processes_completed:
             break
 
-        for i in range(n):
-            if queue[i] != 0:
-                ctr = 0
-                while ctr < tq and temp_burst[queue[0]-1] > 0:
-                    temp_burst[queue[0]-1] -= 1
-                    timer += 1
-                    ctr += 1
-
-                    # Updating the ready queue until all the processes arrive
-                    checkNewArrival(timer, arrival, n, maxProccessIndex, queue)
-
-                if temp_burst[queue[0]-1] == 0 and not complete[queue[0]-1]:
-                    # turn currently stores exit times
-                    turn[queue[0]-1] = timer
-                    complete[queue[0]-1] = True
-
-                # checks whether or not CPU is idle
-                idle = True
-                for k in range(n):
-                    if queue[k] != 0 and not complete[queue[k]-1]:
-                        idle = False
-                        break
-
-                if idle:
-                    timer += 1
-                    checkNewArrival(timer, arrival, n, maxProccessIndex, queue)
-
-                # Maintaining the entries of processes after each preemption in the ready Queue
-                queueMaintainence(queue, n)
-
-    for i in range(n):
-        turn[i] = turn[i] - arrival[i]
-        wait[i] = turn[i] - burst[i]
+    average_waiting_time = sum(waiting_time) / len(waiting_time)
 
     print("\nRound Robin Scheduling Results:")
-    for i in range(n):
-        print(f"P[{processes[i][0]}] start time: {arrival[i]} end time: {turn[i] + arrival[i]} | Waiting time: {wait[i]}")
+    for i in range(len(processes)):
+        print(f"P[{processes[i][0]}] start time: {completion_time[i] - processes[i][2]} end time: {completion_time[i]} | Waiting time: {waiting_time[i]}")
 
-    for i in range(n):
-        avgWait += wait[i]
-        avgTT += turn[i]
+    print("Average waiting time:", average_waiting_time)
 
-    print("\nAverage wait time : ", (avgWait//n))
-    print("\nAverage Turn Around Time : ", (avgTT//n))
+    print("\nGantt Chart:")
+    print("|", end="")
+    for item in gantt_chart:
+        print(f" P{item} |", end="")
+    print()
+
+if __name__ == '__main__':
+    file_name = input("Enter the name of the input text file: ")
+    input_data = process_input_from_file(file_name)
+
+    if input_data:
+        x, n, q, processes = input_data
+        
+        if x == 0:
+            fcfs_scheduling(processes)
+        elif x == 1:
+            sjf_scheduling(processes)
+        elif x == 2:
+            srtf_scheduling(processes)
+        elif x == 3:
+            round_robin_scheduling(processes, q)
+        else:
+            print("Unsupported scheduling algorithm.")
 
 
 if __name__ == '__main__':
